758d09ffdbf7426949ad36242b3d85940b8170c6
Arinya27 <33674162+Arinya27@users.noreply.github.com>
2017-11-15 18:48:52 -0600

    Update database.js

--- Product Details/js/database.js
+++ Product Details/js/database.js
@@ -17,7 +17,7 @@ function populateDB(tx) {
 	
 	tx.executeSql('INSERT INTO EMPLOYEE (id, pName,pDesc,pPrice,pImage) VALUES (0, "iBall Rocky Clarity Headsets","1 Year Manufacturer warranty","$12.21","prd_img1.png")');
 	
-	tx.executeSql('INSERT INTO EMPLOYEE (id, pName,pDesc,pPrice,pImage) VALUES (1, "Sandisk USB Pendrive","32GB Slider","$ 15.30","prd_img2.png")');
+	tx.executeSql('INSERT INTO EMPLOYEE (id, pName,pDesc,pPrice,pImage) VALUES (1, "Sandisk USB Pendrive","32GB Slider","$ 15.30","prd_img1.png")');
 	
 	tx.executeSql('INSERT INTO EMPLOYEE (id, pName,pDesc,pPrice,pImage) VALUES (2, "Enter USB 2.0 Speaker","6 Months Vendor Warranty","$20.00","prd_img3.png")');
 	
